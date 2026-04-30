6e870b6c9d20bd00edc03e40e9b0a94e68b3275c
kawachif <kawachif@C010423162.zipangu.cac.co.jp>
2026-04-30 12:30:52 +0900

    q01完了
    理解できた点
    ・try文の流れ
    ・privateクラスのメソッドの呼び出し
    
    課題点
    ・自分で構造を考えコーディングすること

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/Member.java
@@ -0,0 +1,91 @@
+package q01_basic.question05;
+
+public class Member extends AbstMember {
+
+	private int id;
+	private String password;
+	private int age;
+	private int rank;
+
+	/**
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id セットする id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password セットする password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age セットする age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return rank
+	 */
+	public int getRank() {
+		return rank;
+	}
+
+	/**
+	 * @param rank セットする rank
+	 */
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public Member() {
+
+	}
+
+	public Member(int id, String password, String name, int age, int rank) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+	}
+
+	public void buyItem() {
+		System.out.println(name + "purchased the item at 50% off");
+	}
+
+	public void showMember() {
+		System.out.println("***MEMBER DATA***");
+		System.out.println("id:" + id);
+		System.out.println("password:" + password);
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		System.out.println("rank:" + rank);
+		System.out.println("*******");
+	}
+}
